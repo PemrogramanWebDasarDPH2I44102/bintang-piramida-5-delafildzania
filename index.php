4018142fe4b3d9d5f6831028746e7aedddd26956
delafildzania <42663150+delafildzania@users.noreply.github.com>
2018-09-13 08:22:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
-//tuliskan loopingnya di sini
-for($i=5; $i>=1; $i--){
-for($j=1; $j<=$i; $j++){
-echo "*";
+echo"<b>Bintang Piramida dengan menggunakan fungsi Rekursif</b><br><br>";
+function rekursif ($batas){
+	echo "<pre>";
+	for ($i=1;$i<=$batas;$i++){
+     for ($j=$batas;$j>=$i;$j--){
+    echo "*";
+      }
+     echo "<br>";
 }
-echo"<br>";
+	echo "</pre>";
 }
+
+rekursif(5);
+
 
 ?>
